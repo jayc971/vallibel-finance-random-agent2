00cca49f1a812cdeed09153b8f1b23c1d1432e4d
Jayanika Chandrapriya <jayanika1997@gmail.com>
2021-01-29 13:06:49 +0530

    fsdds

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ include 'Router.php';
 $request = $_SERVER['REQUEST_URI'];
 $router = new Router($request);
 
-$router->get('/', 'app/agents/index');
-$router->get('names', 'app/names/index');
+$router->get('/', 'app/agents');
+$router->get('names', 'app/names');
