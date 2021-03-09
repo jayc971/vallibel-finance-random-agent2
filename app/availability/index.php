35b42e4defcb2a685d8dabe9ac9668ba5078c8a9
jayc971 <jayanika1997@gmail.com>
2021-03-09 13:21:47 +0530

    availability

--- app/availability/index.php
+++ app/availability/index.php
@@ -1,6 +1,6 @@
 <?php
 
-if (time() < strtotime('7 pm')) {
+if (time() < strtotime('7 am')) {
   echo '<script>alert("7 is not passed")</script>'; 
 }else{
 	echo '<script>alert("7 is passed")</script>'; 
