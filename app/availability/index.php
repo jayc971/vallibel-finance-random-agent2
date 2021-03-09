d865db17aa9e76a100b003f739a6e39474661db7
jayc971 <jayanika1997@gmail.com>
2021-03-09 13:47:49 +0530

    availability

--- app/availability/index.php
+++ app/availability/index.php
@@ -4,8 +4,6 @@ $image_folder = "images";
 
 if (time() < strtotime('10:30 am')) {
   header('Location: ' . $image_folder.'/online.png');
-}else{
-	echo '<script>alert("7 is passed")</script>'; 
 }
  
 ?>
